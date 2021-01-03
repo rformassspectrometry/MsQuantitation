76d4095fcf549962d3fdf15b27ec358eb56d4aa8
Laurent Gatto <laurent.gatto@uclouvain.be>
2021-01-03 18:48:39 +0100

    refact: support user-provided acquisitionNum and precScanNum

--- R/quantitation-labelled-ms3.R
+++ R/quantitation-labelled-ms3.R
@@ -6,13 +6,45 @@
 #'
 #' @importFrom methods as
 #'
+#' @details
+#'
+#' While quantitation is performed at the MS3 level to populate the
+#' assay slot, the SummarizedExperiment's rowData stems from the
+#' MS2-level data. The matching between the MS3 scans and their
+#' corresponding precursor MS2 scans is performed by matching two
+#' variables of `spectraData(x)`. These two variables correspond to
+#' the characters defined in `acquistionNum` and `precScanNum`, that
+#' by default contain `"acquisitionNum"` (the acquisition numbers of
+#' all spectra) and `"precScanNum"` (the acquisition number of
+#' precursor scans). These defaults work with standard `Spectra`
+#' objects.
+#'
+#' @param x A `Spectra` object.
+#'
+#' @param reporters A `ReporterIons` object.
+#'
+#' @param acquistionNum `character(1)` rowData column name defining all scan
+#'     acquisition numbers. Default is `"acquistionNum"`.
+#'
+#' @param precScanNum `character(1)` rowData column name defining the
+#'     precursor scan acquisition numbers . Default is
+#'     `"precScanNum"`.
+#'
+#' @param ... Additional parameters passed to
+#'     [BiocParallel::bplally()].
+#'
 #' @noRd
-quantify_1_labelled_ms3 <- function(x, reporters, ...) {
+quantify_1_labelled_ms3 <- function(x, reporters,
+                                    acquistionNum = "acquisitionNum",
+                                    precScanNum = "precScanNum",
+                                    ...) {
+    stopifnot(length(acquisitionNum) == 1)
+    stopifnot(length(precScanNum) == 1)
     x3 <- filterMsLevel(x, 3L)
     pks <- peaksData(x3)
     ans <- quantifyPeakMatrixList(pks, mz(reporters), w = width(reporters), ...)
     colnames(ans) <- reporterNames(reporters)
-    i <- match(x3$precScanNum, x$acquisitionNum)
+    i <- match(x3[[precScanNum]], x[[acquistionNum]])
     SummarizedExperiment(assays = SimpleList(ans),
                          rowData = spectraData(x)[i, ],
                          colData = as(reporters, "DataFrame"))
