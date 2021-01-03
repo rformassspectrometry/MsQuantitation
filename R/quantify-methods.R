5e6671a22445b7f2500a0e8a300d52c5111b2299
Laurent Gatto <laurent.gatto@uclouvain.be>
2021-01-03 10:35:21 +0100

    fix: resolve warnings for quantify man page

--- R/quantify-methods.R
+++ R/quantify-methods.R
@@ -2,12 +2,21 @@
 #'
 #' @description
 #'
+#' The `quantify()` method is the entry point for quantitation of raw
+#' mass spectrometry data. The raw data is contained in a [Spectra()]
+#' object while the details and parameters of the quantitation method
+#' are defined in a dedicated [QuantParam()].
+#'
+#' @aliases quantify quantify,Spectra
+#'
 #' @rdname quantify-methods.Rd
 #'
 #' @param object An instance of class [Spectra()].
 #'
 #' @param param An instance of class [QuantParam()] that defines the
-#'     quantitation method. 
+#'     quantitation method.
+#'
+#' @param ... additional parameters (currently ignored).
 #' 
 #' @return An instance of [QFeatures()].
 #' 
